04194eb99ab14d04614cc2cecb394cf56084ea49
Giullia Beatriz <giulliabeatrizrsilva@gmail.com>
2023-10-26 23:37:51 -0400

    nomes alterados e logins inclusos no readme

--- README.mk
+++ README.mk
@@ -3,6 +3,7 @@ Alunos: Giullia Beatriz Ramos e Silva, Rodrigo Fabio Lima de Amorim
 *** SENHAS PARA REALIZAR LOGIN ***
 usuario: usuario1 - senha: senha1
 usuario: usuario2 - senha: senha2 
+usuario: usuario3 - senha: senha3
 
 REFERÊNCIAS
 statement, bind value, parametros em consultas sql para evitar SQL injection: https://www.php.net/manual/en/pdostatement.bindvalue.php
